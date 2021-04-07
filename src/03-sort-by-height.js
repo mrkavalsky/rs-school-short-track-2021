fa3992158a21c940a25f9c72a9a73b58bbb4be21
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-07 13:37:41 +0300

    feat: implement script for sort-by-height subtask

--- src/03-sort-by-height.js
+++ src/03-sort-by-height.js
@@ -9,8 +9,19 @@
  *
  * The result should be [-1, 150, 160, 170, -1, -1, 180, 190]
  */
-function sortByHeight(/* arr */) {
-  throw new Error('Not implemented');
+function sortByHeight(arr) {
+  const posNum = arr.filter((item) => item !== -1);
+  const res = [];
+  posNum.sort((a, b) => {
+    if (a < b) return 1;
+    if (a === b) return 0;
+    return -1;
+  });
+  arr.forEach((item) => {
+    if (item === -1) res.push(-1);
+    else res.push(posNum.pop());
+  });
+  return res;
 }
 
 module.exports = sortByHeight;
