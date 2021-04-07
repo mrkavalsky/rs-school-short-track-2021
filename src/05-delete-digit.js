7525ff8ce442530012d2c32a21a1c209ce76b1a4
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-07 16:17:08 +0300

    feat: implement script for delete-digit subtask

--- src/05-delete-digit.js
+++ src/05-delete-digit.js
@@ -9,8 +9,22 @@
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new Error('Not implemented');
+function deleteDigit(n) {
+  const string = `${n}`;
+  const arr = string.split('');
+  let result = [];
+  let pos = 0;
+  for (let i = 0; i <= 9; i++) {
+    if (arr.find((elem) => i === +elem)) {
+      pos = arr.findIndex((item) => i === +item);
+      break;
+    }
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (i !== pos) result.push(arr[i]);
+  }
+  result = result.join('');
+  return +result;
 }
 
 module.exports = deleteDigit;
