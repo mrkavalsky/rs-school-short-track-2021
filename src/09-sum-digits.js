8a34770233ae2d01155d2fb6c746e6e37faae36d
mrkavalsky <mr.kavalsky@gmail.com>
2021-04-07 21:04:37 +0300

    feat: implement script for sum-digits subtask

--- src/09-sum-digits.js
+++ src/09-sum-digits.js
@@ -10,8 +10,14 @@
  * For 91, the result should be 1 (9 + 1 = 10, 1 + 0 = 1)
  *
  */
-function getSumOfDigits(/* n */) {
-  throw new Error('Not implemented');
+function getSumOfDigits(n) {
+  let string = n.toString();
+  while (string.length !== 1) {
+    const arr = string.split('');
+    string = arr.reduce((sum, current) => sum + +current, 0);
+    string = string.toString();
+  }
+  return +string;
 }
 
 module.exports = getSumOfDigits;
